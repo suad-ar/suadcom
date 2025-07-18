1a9a3f79b2314f910f3b4575bfaba4f944c4bbcb
Suad AR <suadrahman@live.com>
2025-07-18 21:12:31 +0000

    case study ui and content edits

--- src/app/satoreality/satoreality.component.ts
+++ src/app/satoreality/satoreality.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-satoreality',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './satoreality.component.html',
   styleUrl: './satoreality.component.css'
 })
